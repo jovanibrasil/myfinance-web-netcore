b7a551bd1ba86c078398355c9eb6934f8c5b0433
Jovani Brasil <jovanibrasil@gmail.com>
2024-12-21 13:33:09 -0300

    add database url into appsettings config

--- myfinance-web-dotnet-infra/MyFinanceDbContext.cs
+++ myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -1,15 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using myfinance_web_dotnet_domain.Entities;
 
 namespace myfinance_web_dotnet_infra;
 public class MyFinanceDbContext : DbContext
 {
+  private readonly IConfiguration _configuration;
+
+  public MyFinanceDbContext(IConfiguration configuration)
+  {
+    _configuration = configuration;
+  }
+
   public DbSet<Category> Category { get; set; }
   public DbSet<FinancialTransaction> FinancialTransaction { get; set; }
 
   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
   {
-    optionsBuilder.UseSqlServer(@"Server=localhost;Database=myFinance;User Id=sa;Password=YourStrongPassword1!;Trusted_Connection=False;Encrypt=False;TrustServerCertificate=True");
+    optionsBuilder.UseSqlServer(_configuration.GetConnectionString("Database"));
   }
 
 }
